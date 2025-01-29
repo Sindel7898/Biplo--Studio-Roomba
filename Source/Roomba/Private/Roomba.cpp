38732340c5437eae789809e0d6add388a23e44e5
Amir Sanni <sanniamir538@gmail.com>
2025-01-29 14:56:24 +0000

    Implement light detection system
    - Added ALightDetection class to measure brightness in a scene
    - Implemented texture-based brightness calculation using RenderTarget
    - Added real-time debug messages to display brightness levels
    - Optimized memory allocation for pixel storage
    - Ensured periodic updates every 10 frames to improve performance"

--- Source/Roomba/Private/Roomba.cpp
+++ Source/Roomba/Private/Roomba.cpp
@@ -1,6 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "Roomba.h"
+#include "Roomba/Public/Roomba.h"
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Roomba, "Roomba" );
---
+++ Source/Roomba/Public/LightDetection.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "UnrealClient.h"
+#include "LightDetection.generated.h"
+
+UCLASS()
+class  ALightDetection : public AActor
+{
+	GENERATED_BODY()
+	
+private:	
+	// Sets default values for this actor's properties
+	ALightDetection();
+
+	UFUNCTION()
+	float CalculateBrightness();
+
+	void ProcessRenderTexture(UTextureRenderTarget2D *Texture);
+	
+	TArray<FColor> PixelStorage;
+	
+	float PixelChannelR = 0;
+	float PixelChannelG = 0;
+	float PixelChannelB = 0;
+	
+	float BrightnessOutput = 0;
+	float CurrentPixelBrightness = 0;
+
+	float TotalBrightness = 0;
+	int   TotalPixelCount = 0;
+
+	FRenderTarget *RenderTarget = nullptr;
+	
+	UPROPERTY(EditAnywhere)
+	class UTextureRenderTarget2D *DetectorTextureTop = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	bool BLighting = false;
+	float Brightness = 0;
+	bool IsPlayerInLight = false;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	FORCEINLINE bool GetBIsPlayerInLight() const {return IsPlayerInLight;}
+};
