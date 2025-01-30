c3fbb1008b6f9c466b2394b1aec21c81ffcfd1a5
Abertay2203093 <124072006+Abertay2203093@users.noreply.github.com>
2025-01-30 12:52:36 +0000

    not tested yet.. conversion to cpp

---
+++ Source/Roomba/Private/Drone.cpp
@@ -0,0 +1,99 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Drone.h"
+
+
+// Sets default values
+ADrone::ADrone()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Setup configurations
+	MaxThrust = 300.0f;
+	Acceleration = 5.0f;
+
+	CurrentForwardThrust = 0.0f;
+	CurrentRightThrust = 0.0f;
+
+	// Setup components
+	DroneRootCube = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DroneRootCube"));
+	DroneRootCube->SetAngularDamping(1);
+	DroneRootCube->SetSimulatePhysics(true);
+	DroneRootCube->SetHiddenInGame(true);
+	DroneRootCube->SetEnableGravity(false);
+	RootComponent = DroneRootCube;
+
+	DroneStaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DroneStaticMesh"));
+	DroneStaticMesh->SetupAttachment(RootComponent);
+
+	PrimarySpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("PrimarySpringArm"));
+	PrimarySpringArm->SetupAttachment(RootComponent);
+	PrimarySpringArm->TargetArmLength = 65.0f;
+	PrimarySpringArm->bEnableCameraLag = true;
+	
+	PrimaryCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("PrimaryCamera"));
+	PrimaryCamera->SetupAttachment(PrimarySpringArm);
+	PrimaryCamera->SetRelativeLocation(FVector(-150.0f, 0.0f, 155.0f));
+	PrimaryCamera->SetRelativeRotation(FQuat::MakeFromEuler(FVector(0.0f, -25.0f, 0.0f)));
+	PrimaryCamera->bAutoActivate = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADrone::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+	
+}
+
+float ADrone::GetDirectionSpeedMethod(float DeltaTime, float InputAxisValue, float CurrentSpeed)
+{
+	const float Alpha = (InputAxisValue + 1.0f) / 2.0f;
+	const float Lerp = FMath::Lerp(-MaxThrust, MaxThrust, Alpha);
+	const float InterpSpeed = Alpha * Acceleration;
+
+	const float NewSpeed = FMath::FInterpTo(CurrentSpeed, Lerp, DeltaTime, InterpSpeed);
+	return NewSpeed;
+}
+
+
+FVector ADrone::GetMovementVelocityMethod(float DeltaTime)
+{
+	const float MoveRight = InputComponent->GetAxisValue("MoveRight");
+	const float MoveForward = InputComponent->GetAxisValue("MoveForward");
+	
+	const float ForwardSpeed = GetDirectionSpeedMethod(DeltaTime, MoveForward, CurrentForwardThrust);
+	const float RightSpeed = GetDirectionSpeedMethod(DeltaTime, MoveRight, CurrentRightThrust);
+
+	CurrentRightThrust = RightSpeed;
+	CurrentForwardThrust = ForwardSpeed;
+
+	const FVector ForwardVector = DroneRootCube->GetForwardVector() * CurrentForwardThrust;
+	const FVector RightVector = DroneRootCube->GetRightVector() * CurrentRightThrust;
+
+	return ForwardVector + RightVector;
+}
+
+
+// Called every frame
+void ADrone::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector NewVelocity = GetMovementVelocityMethod(DeltaTime);
+	DroneRootCube->SetPhysicsLinearVelocity(NewVelocity);
+}
+
+// Called to bind functionality to input
+void ADrone::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	
+
+}
+
---
+++ Source/Roomba/Public/Drone.h
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Components/SceneComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Drone.generated.h"
+
+UCLASS()
+class ROOMBA_API ADrone : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ADrone();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// TODO DESCRIPTION
+	FVector GetMovementVelocityMethod(float DeltaTime);
+
+	// TODO DESCRIPTION
+	float GetDirectionSpeedMethod(float DeltaTime, float InputAxisValue, float CurrentSpeed);
+
+	
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Root cube is the actual physics parts of the drone. This allows it to not be tied
+	// into what mesh the artists are designing and their mass, scale, etc
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DroneComponents")
+	UStaticMeshComponent* DroneRootCube;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DroneComponents")
+	UStaticMeshComponent* DroneStaticMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DroneComponents")
+	USpringArmComponent* PrimarySpringArm;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DroneComponents")
+	UCameraComponent* PrimaryCamera;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MovementConfiguration")
+	float MaxThrust;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MovementConfiguration")
+	float Acceleration;
+
+	// TODO DESCRIPTION
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "LiveStats");
+	float CurrentForwardThrust;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "LiveStats");
+	float CurrentRightThrust;
+
+	
+};
